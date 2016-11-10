faf1afcd41afb4d02cdb065fdff408aeba1507d0
Eugene Lutz <elutz@weezlabs.com>
2016-11-10 13:11:26 +0300

    migration script was rewritten using ES6

--- createDB.js
+++ createDB.js
@@ -1,180 +1,120 @@
 'use strict';
-const async = require('async');
-const mongoose = require('./lib/mongoose');
-
-async.series([
-    open,
-    dropDatabase,
-    requireModels,
-    createUsers,
-    createWords
-], function(err, results){
-    disconnect();
-    console.log(arguments);
-});
-
-function open(callback){
-    mongoose.connection.on('open', callback);
-}
-
-function dropDatabase(callback){
-    let db = mongoose.connection.db;
-    db.dropDatabase(callback);
-}
-
-function requireModels(callback){
-
-    async.each(Object.keys(mongoose.models), (modelName, callback) => {
-        mongoose.models[modelName].ensureIndexes(callback);
-    }, callback);
-}
-
-function createUsers(callback){
-    const User = require('./models/user').User;
-    let users = [
-        new User({username: 'Vasya', password: '2342314'}),
-        new User({username: 'Petya', password: '2344'}),
-        new User({username: 'Admin', password: '234qwef4'})
-    ];
-
-    async.each(users, (userData, callback) => {
-        let user = new User(userData);
-        user.save(callback);
-    }, callback);
-}
-
-function createWords(callback){
-    const Word = require('./models/word').Word;
-
-    let words = [
-        new Word({engword: 'angle', ruswords: [ 'угол']}),
-        new Word({engword: 'ant', ruswords: ['муравей']}),
-        new Word({engword: 'apple', ruswords: ['яблоко']}),
-        new Word({engword: 'arch', ruswords: ['арка', 'дуга', 'выгибать']}),
-        new Word({engword: 'arm', ruswords: ['рука']}),
-        new Word({engword: 'army', ruswords: ['армия']}),
-        new Word({engword: 'bag', ruswords: ['сумка']}),
-        new Word({engword: 'ball', ruswords: ['мяч']}),
-        new Word({engword: 'bank', ruswords: ['банк']}),
-        new Word({engword: 'basin', ruswords: ['бассейн']}),
-        new Word({engword: 'basket', ruswords: ['корзина']}),
-        new Word({engword: 'bath', ruswords: ['ванна', 'купаться']}),
-        new Word({engword: 'bed', ruswords: ['кровать']}),
-        new Word({engword: 'bee', ruswords: ['пчела']}),
-        new Word({engword: 'bell', ruswords: ['колокол']}),
-        new Word({engword: 'berry', ruswords: ['ягода']}),
-        new Word({engword: 'bird', ruswords: ['птица']}),
-        new Word({engword: 'blade', ruswords: ['лезвие']}),
-        new Word({engword: 'board', ruswords: ['доска']}),
-        new Word({engword: 'boat', ruswords: ['лодка', 'судно']}),
-        new Word({engword: 'bone', ruswords: ['ключ']}),
-        new Word({engword: 'book', ruswords: ['книга']}),
-        new Word({engword: 'boot', ruswords: ['ботинок', 'загружать']}),
-        new Word({engword: 'bottle', ruswords: ['бутылка']}),
-        new Word({engword: 'box', ruswords: ['коробка']}),
-        new Word({engword: 'boy', ruswords: ['мальчик']}),
-        new Word({engword: 'brain', ruswords: ['мозг']}),
-        new Word({engword: 'brake', ruswords: ['тормоз', 'тормозить']}),
-        new Word({engword: 'branch', ruswords: ['ветка', 'отделение']}),
-        new Word({engword: 'brick', ruswords: ['кирпич']}),
-        new Word({engword: 'bridge', ruswords: ['мост']}),
-        new Word({engword: 'brush', ruswords: ['кисть', 'щётка']}),
-        new Word({engword: 'bucket', ruswords: ['ведро', 'черпать']}),
-        new Word({engword: 'bulb', ruswords: ['луковица', 'выпирать']}),
-        new Word({engword: 'button', ruswords: ['кнопка', 'застегивать']}),
-        new Word({engword: 'baby', ruswords: ['младенец', 'ребенок']})
-    ];
-
-    async.each(words, (wordData, callback) => {
-        let word = new Word(wordData);
-        word.save(callback);
-    }, callback);
-}
-
-function disconnect(callback){
-    mongoose.disconnect(callback);
-}
-
-/*'use strict';
  const mongoose = require('./lib/mongoose');
  const User = require('./models/user').User;
  const Word = require('./models/word').Word;
 
- new Promise(resolve => {
- mongoose.connection.on('open', resolve);
- })
- .then(() => {
- mongoose.connection.db.dropDatabase(function(){});
- })
- .then(() => {
- let models = Object.keys(mongoose.models);
- models.forEach(modelName => {
- mongoose.models[modelName].ensureIndexes();
- });
- })
- .then(() => {
- let users = [
- new User({username: 'Vasya', password: '2342314'}),
- new User({username: 'Petya', password: '2344'}),
- new User({username: 'Admin', password: '234qwef4'})
- ];
+ function connect(){
+     console.log('connect open');
+     return new Promise(resolve => {
+         mongoose.connection.on('open', resolve);
+     });
+ }
 
- users.forEach(userData => {
- let user = new User(userData);
- user.save();
- });
- /*
- let words = [
- new Word({engword: 'angle', ruswords: [ 'угол']}),
- new Word({engword: 'ant', ruswords: ['муравей']}),
- new Word({engword: 'apple', ruswords: ['яблоко']}),
- new Word({engword: 'arch', ruswords: ['арка', 'дуга', 'выгибать']}),
- new Word({engword: 'arm', ruswords: ['рука']}),
- new Word({engword: 'army', ruswords: ['армия']}),
- new Word({engword: 'bag', ruswords: ['сумка']}),
- new Word({engword: 'ball', ruswords: ['мяч']}),
- new Word({engword: 'bank', ruswords: ['банк']}),
- new Word({engword: 'basin', ruswords: ['бассейн']}),
- new Word({engword: 'basket', ruswords: ['корзина']}),
- new Word({engword: 'bath', ruswords: ['ванна', 'купаться']}),
- new Word({engword: 'bed', ruswords: ['кровать']}),
- new Word({engword: 'bee', ruswords: ['пчела']}),
- new Word({engword: 'bell', ruswords: ['колокол']}),
- new Word({engword: 'berry', ruswords: ['ягода']}),
- new Word({engword: 'bird', ruswords: ['птица']}),
- new Word({engword: 'blade', ruswords: ['лезвие']}),
- new Word({engword: 'board', ruswords: ['доска']}),
- new Word({engword: 'boat', ruswords: ['лодка', 'судно']}),
- new Word({engword: 'bone', ruswords: ['ключ']}),
- new Word({engword: 'book', ruswords: ['книга']}),
- new Word({engword: 'boot', ruswords: ['ботинок', 'загружать']}),
- new Word({engword: 'bottle', ruswords: ['бутылка']}),
- new Word({engword: 'box', ruswords: ['коробка']}),
- new Word({engword: 'boy', ruswords: ['мальчик']}),
- new Word({engword: 'brain', ruswords: ['мозг']}),
- new Word({engword: 'brake', ruswords: ['тормоз', 'тормозить']}),
- new Word({engword: 'branch', ruswords: ['ветка', 'отделение']}),
- new Word({engword: 'brick', ruswords: ['кирпич']}),
- new Word({engword: 'bridge', ruswords: ['мост']}),
- new Word({engword: 'brush', ruswords: ['кисть', 'щётка']}),
- new Word({engword: 'bucket', ruswords: ['ведро', 'черпать']}),
- new Word({engword: 'bulb', ruswords: ['луковица', 'выпирать']}),
- new Word({engword: 'button', ruswords: ['кнопка', 'застегивать']}),
- new Word({engword: 'baby', ruswords: ['младенец', 'ребенок']})
- ];
+ function dropDb(){
+     console.log('dropDb');
+     return new Promise(resolve => {
+         mongoose.connection.db.dropDatabase(resolve);
+     });
+ }
 
- //http://iloveenglish.ru/stories/view/basic_english_ch_2_850_samikh_neobkhodimikh_anglijskikh_slov
+ function ensureModelIndexes() {
+     console.log('ensureModelIndexes');
+     let models = Object.keys(mongoose.models);
 
- words.forEach(wordData => {
- let word = new Word(wordData);
- word.save();
- });
- })
- .then(() => {
- mongoose.disconnect();
- })
+     for (let modelName of models) {
+         mongoose.models[modelName].ensureIndexes();
+     }
+ }
+
+ function createUsers() {
+     console.log('createUsers');
+
+     let users = [
+         new User({username: 'Vasya', password: '2342314'}),
+         new User({username: 'Petya', password: '2344'}),
+         new User({username: 'Admin', password: '234qwef4'})
+     ];
+
+     let userSaveFunctions = users.map(userData => new Promise((resolve, reject) => {
+         let user = new User(userData);
+         user.save(err => {
+             console.log('user saved');
+             if(err) reject(err);
+             resolve();
+         });
+     }));
+
+     return Promise.all(userSaveFunctions);
+ }
+
+ function createWords() {
+     console.log('createWords');
+
+     let words = [
+         new Word({engword: 'angle', ruswords: [ 'угол']}),
+         new Word({engword: 'ant', ruswords: ['муравей']}),
+         new Word({engword: 'apple', ruswords: ['яблоко']}),
+         new Word({engword: 'arch', ruswords: ['арка', 'дуга', 'выгибать']}),
+         new Word({engword: 'arm', ruswords: ['рука']}),
+         new Word({engword: 'army', ruswords: ['армия']}),
+         new Word({engword: 'bag', ruswords: ['сумка']}),
+         new Word({engword: 'ball', ruswords: ['мяч']}),
+         new Word({engword: 'bank', ruswords: ['банк']}),
+         new Word({engword: 'basin', ruswords: ['бассейн']}),
+         new Word({engword: 'basket', ruswords: ['корзина']}),
+         new Word({engword: 'bath', ruswords: ['ванна', 'купаться']}),
+         new Word({engword: 'bed', ruswords: ['кровать']}),
+         new Word({engword: 'bee', ruswords: ['пчела']}),
+         new Word({engword: 'bell', ruswords: ['колокол']}),
+         new Word({engword: 'berry', ruswords: ['ягода']}),
+         new Word({engword: 'bird', ruswords: ['птица']}),
+         new Word({engword: 'blade', ruswords: ['лезвие']}),
+         new Word({engword: 'board', ruswords: ['доска']}),
+         new Word({engword: 'boat', ruswords: ['лодка', 'судно']}),
+         new Word({engword: 'bone', ruswords: ['ключ']}),
+         new Word({engword: 'book', ruswords: ['книга']}),
+         new Word({engword: 'boot', ruswords: ['ботинок', 'загружать']}),
+         new Word({engword: 'bottle', ruswords: ['бутылка']}),
+         new Word({engword: 'box', ruswords: ['коробка']}),
+         new Word({engword: 'boy', ruswords: ['мальчик']}),
+         new Word({engword: 'brain', ruswords: ['мозг']}),
+         new Word({engword: 'brake', ruswords: ['тормоз', 'тормозить']}),
+         new Word({engword: 'branch', ruswords: ['ветка', 'отделение']}),
+         new Word({engword: 'brick', ruswords: ['кирпич']}),
+         new Word({engword: 'bridge', ruswords: ['мост']}),
+         new Word({engword: 'brush', ruswords: ['кисть', 'щётка']}),
+         new Word({engword: 'bucket', ruswords: ['ведро', 'черпать']}),
+         new Word({engword: 'bulb', ruswords: ['луковица', 'выпирать']}),
+         new Word({engword: 'button', ruswords: ['кнопка', 'застегивать']}),
+         new Word({engword: 'baby', ruswords: ['младенец', 'ребенок']})
+     ];
+
+     //http://iloveenglish.ru/stories/view/basic_english_ch_2_850_samikh_neobkhodimikh_anglijskikh_slov
+
+     let wordSaveFunctions = words.map(wordData => new Promise((resolve, reject) => {
+         let word = new Word(wordData);
+         word.save(err => {
+             if(err) reject(err);
+             resolve();
+         });
+     }));
+
+     return Promise.all(wordSaveFunctions);
+ }
+
+ function disconnect() {
+     console.log('connection close');
+     mongoose.disconnect();
+ }
+
+ connect()
+ .then(dropDb)
+ .then(ensureModelIndexes)
+ .then(createUsers)
+ .then(createWords)
+ .then(disconnect)
  .catch(err => {
- mongoose.disconnect();
- console.log(err);
+     console.log('catch you!');
+     disconnect();
+     console.log(err);
  });
- */
